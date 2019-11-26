0bb029c2d93638e5dc876a02414bf3d4f6f23972
Xu Wang <xuwang@gmail.com>
2019-11-25 21:48:52 -0800

    Add file2configmap.sh.

---
+++ file2configmap.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# MIT License
+# 
+# Copyright (c) 2019 Xu Wang
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Usage:
+#   file2configmap <namespace> <map-name> <from-file>
+# Read content from <from-file> and convert it to Kubernetes configmap resource.
+
+OLDIFS=$IFS
+IFS=''
+
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $(basenae $0) <namespace> <map-name> <map-key> [<from-file>]"
+    exit 1
+fi
+# Required arguments
+set -u
+APP_NAMESPACE=$1
+MAP_NAME=$2
+KEY=$3
+set +u
+
+cat <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: ${MAP_NAME}
+  namespace: ${APP_NAMESPACE}
+data:
+  ${KEY}: |+
+EOF
+while read line
+do
+    echo "    $line"
+done < "${4:-/dev/stdin}"
+IFS=$OLDIFS
